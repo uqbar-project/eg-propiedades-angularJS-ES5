79ac72aa4ef6f8336a91a4e75695fd289ec99fb9
fdodino <fernando.dodino@gmail.com>
2016-09-20 11:55:59 -0300

    Adding new branch explaining ng-include

--- app/routes.js
+++ app/routes.js
@@ -4,24 +4,14 @@ angular.module('booking-app', ['ui.router'])
 .config(function($urlRouterProvider, $locationProvider) {
   $urlRouterProvider.otherwise('/');
   return $locationProvider.html5Mode(true);
-})
+});
+angular.module('booking-app')
 .config(function($stateProvider) {
   return $stateProvider
-  .state('main', {
-    abstract: true,
-    views: {
-      'layout': {
-        templateUrl: "app/layout/layout.html"
-      },
-      'container@main': {
-        template: "<ui-view>"
-      },
-      'topbar@main': {
-        templateUrl: "app/layout/topbar.html"
-      },
-      'sidebar@main': {
-        templateUrl: "app/layout/sidebar.html"
-      }
-    }
+  .state('dashboard', {
+    url: "/",
+    templateUrl: "app/modules/dashboard/dashboard.html",
+    controller: "DashboardCtrl",
+    controllerAs:"dashboardCtrl"
   });
 });
